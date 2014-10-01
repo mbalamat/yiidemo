a4ca804fa484270601f72684ac09a3dce68e5088
mbalamat <mbalamatsias@gmail.com>
2014-10-01 20:44:09 +0300

    DB added for testing

---
+++ database/apointments_Appointments.sql
@@ -0,0 +1,55 @@
+CREATE DATABASE  IF NOT EXISTS `apointments` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `apointments`;
+-- MySQL dump 10.13  Distrib 5.5.38, for debian-linux-gnu (x86_64)
+--
+-- Host: 127.0.0.1    Database: apointments
+-- ------------------------------------------------------
+-- Server version	5.5.38-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Appointments`
+--
+
+DROP TABLE IF EXISTS `Appointments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Appointments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` date NOT NULL,
+  `place` varchar(45) NOT NULL DEFAULT 'Office',
+  `comments` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Appointments`
+--
+
+LOCK TABLES `Appointments` WRITE;
+/*!40000 ALTER TABLE `Appointments` DISABLE KEYS */;
+INSERT INTO `Appointments` VALUES (1,'2014-09-30','School','Μάθημα στις Πιθανότητες'),(2,'2014-10-04','Cezaria Hotel','Παντελής Παντελίδης Live...!'),(4,'2014-10-01','Office','mail στη Μονάδα Αριστείας ΕΛΛΑΚ Ιωαννίνων'),(5,'2014-10-03','Office','Project για Διαβήτη'),(6,'2014-10-05','Home','Dinner with Relatives');
+/*!40000 ALTER TABLE `Appointments` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-10-01 20:40:41
